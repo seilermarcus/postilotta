8e5aa773d96b5bca2ba190a2c81b2dc5b289f641
MarcusUwezo <marcusseiler@Marcuss-MacBook-Air.local>
2017-09-05 11:20:00 +0200

    rm old www

--- www/session.js
+++
@@ -1,9 +0,0 @@
-
-
-if (sessionStorage.sid === null){
-  con ('session.php', {sid:0}, cbAfter_session);
-}
-function cbAfter_session(th){
-  console.log(th.responseText);
-  sessionStorage.sid = th.responseText
-}
