459c597678131b0647cc9d62a3a3ac075b1cd589
MarcusUwezo <marcusseiler@Marcuss-MacBook-Air.local>
2017-09-07 18:15:48 +0200

    mentioning taiga.io

--- php-jks/www/secure.php
+++ php-jks/www/secure.php
@@ -12,18 +12,22 @@
 <div class="txt">
   <p><i>Infographic coming soon.</i></p>
   <p>
-    <b>In a nutshell:<b><br>
+    <b>In a nutshell:</b><br>
     You don't have to trust us, that we do what we promise. Our code is open source, so everyone can verify this,
      and hundreds of geeks and IT-professionals have already done this (soon).
    </p>
-   <table>
+   <table align="center">
      <tr>
-       <td><img src="pics/agpl_black_70.png"></td>
+       <td align="right"><img src="pics/agpl_black_70.png"></td>
        <td><p>All of our source code is free software<br>under AGPL3 license.</p></td>
      </tr>
      <tr>
        <td><p>All of our source code is open source<br>and available here on <a href="https://github.com/seilermarcus/postilotta" target="_blank"><u>GitHub</u></a>.</p></td>
-       <td><img src="pics/github_70.png"></td>
+       <td><a href="https://github.com/seilermarcus/postilotta" target="_blank"><img src="pics/github_70.png"></a></td>
+       <tr>
+         <td align="right"><a href="https://tree.taiga.io/project/marcusuwezo-postilotta/kanban"><img src="pics/taiga_io_80.png"></a></td>
+         <td>All of our requirements and development process management<br>is open for participation here on <a href="https://tree.taiga.io/project/marcusuwezo-postilotta/kanban" target="_blank"><u>taiga.io</u></a></td>
+       </tr>
      </table>
   <p>
     We can do this without risking system integrity, because we practice <i>privacy by design</i> and don't rely on <i>security through obscurity</i>.
