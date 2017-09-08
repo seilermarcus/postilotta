501fa2909e8524a112a4e6cf469b800582fb18fa
MarcusUwezo <marcusseiler@Marcuss-MacBook-Air.local>
2017-09-08 14:11:48 +0200

    issue#19: 2

---
+++ batch_clearall-jks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+db="pta_$1-sql"
+php="pta_$1-php"
+
+docker stop $db && \
+docker rm $db && \
+docker rmi $db && \
+rm -rf /srv/$db
+
+docker stop $php && \
+docker rm $php && \
+docker rmi $php && \
+sudo rm -rf /srv/$php
+rm php-jks/www/inc/sql-ip.inc
