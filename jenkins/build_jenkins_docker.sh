01ec65497286d748a2707e8ea88b4ce1c68a684f
MarcusUwezo <marcusseiler@Marcuss-MacBook-Air.local>
2017-09-11 19:40:31 +0200

    jenkins sh file update
    build-jks +x

--- jenkins/build_jenkins_docker.sh
+++ jenkins/build_jenkins_docker.sh
@@ -10,7 +10,6 @@ docker run \
     -v jenkins_home:/var/jenkins_home \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(which docker):/usr/bin/docker \
-    -v /var/postilotta/prototype:/var/postilotta/prototype \
     -v /srv:/srv \
     -v /usr/lib/x86_64-linux-gnu/libltdl.so.7:/usr/lib/x86_64-linux-gnu/libltdl.so.7 \
     --env JENKINS_OPTS="--httpPort=80" \
@@ -23,5 +22,6 @@ docker run \
 #    -v $(which docker):/usr/bin/docker \
 #    --expose 8080
 #    --env JENKINS_OPTS="--prefix=/jenkins" \
+#    -v /var/postilotta/prototype:/var/postilotta/prototype \
 
 echo 'pta: done'
